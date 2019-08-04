4300a61e46e5f88ddf7a5ce33ce63d4e0505ecbc
Ricardo Colares <ricardo.colares@osf-global.com>
2019-08-04 16:36:58 -0300

    projeto gobarber frontend

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
-import { Router } from "react-router-dom";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import "./config/ReactotronConfig";
+import './config/ReactotronConfig';
 
-import Routes from "./routes";
-import history from "./services/history";
+import Routes from './routes';
+import history from './services/history';
+
+import { store, persistor } from './store';
+
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <ToastContainer autoClose={3000} />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
+import AvatarInput from './AvatarInput';
 
 export default function Prodfile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de email" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button onClick={handleSignOut} type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
